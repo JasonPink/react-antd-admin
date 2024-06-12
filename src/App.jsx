b8d67eb5fc9a77507efbceeb23d71380d2267458
joker <lee_yemeng@163.com>
2024-06-12 15:34:41 +0800

    fix: Suspense 路由懒加载闪烁

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React, { Suspense, useEffect } from 'react';
-
 import {
     BrowserRouter as Router,
     useRoutes,
     useLocation
 } from 'react-router-dom';
+import { Spin } from 'antd';
 import routes from '@/router';
 
 const AppRouter = () => {
     const routing = useRoutes(routes);
     const location = useLocation();
 
-    useEffect(() => {
-        const currentRoute = routes.find(
-            route => route.path === location.pathname
-        );
-        if (currentRoute && currentRoute.meta && currentRoute.meta.title) {
-            document.title = currentRoute.meta.title;
-        }
-    }, [location]);
+    // useEffect(() => {
+    //     const currentRoute = routes.find(
+    //         route => route.path === location.pathname
+    //     );
+    //     if (currentRoute && currentRoute.meta && currentRoute.meta.title) {
+    //         document.title = currentRoute.meta.title;
+    //     }
+    // }, [location]);
 
-    return <Suspense fallback={<div>Loading...</div>}>{routing}</Suspense>;
+    // return <Suspense fallback={<Spin></Spin>}>{routing}</Suspense>;
+    return useRoutes(routes);
 };
 
 const App = () => (
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,16 +1,25 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import {
     createBrowserRouter,
     RouterProvider,
     Navigate
 } from 'react-router-dom';
+import { Spin } from 'antd';
 import Layout from '@/layouts';
+// import Dashboard from '@/pages/Dashboard';
+// import Users from '@/pages/Users';
+// import Profile from '@/pages/Users/profile';
+// import Userlist from '@/pages/Users/list';
 
-const Dashboard = lazy(() => import('@/pages/Dashboard'));
 const Login = lazy(() => import('@/pages/Login'));
+const Dashboard = lazy(() => import('@/pages/Dashboard'));
 const Users = lazy(() => import('@/pages/Users'));
 const Profile = lazy(() => import('@/pages/Users/profile'));
 const Userlist = lazy(() => import('@/pages/Users/list'));
+
+const lazyCom = element => {
+    return <Suspense fallback={<Spin></Spin>}>{element}</Suspense>;
+};
 
 export const layoutRoutes = [
     {
@@ -19,28 +28,28 @@ export const layoutRoutes = [
     },
     {
         path: 'dashboard',
-        element: <Dashboard />,
+        element: lazyCom(<Dashboard />),
         meta: {
             title: '数据看板'
         }
     },
     {
         path: 'user',
-        element: <Users />,
+        element: lazyCom(<Users />),
         meta: {
             title: '用户管理'
         },
         children: [
             {
                 path: 'profile',
-                element: <Profile />,
+                element: lazyCom(<Profile />),
                 meta: {
                     title: '用户简介'
                 }
             },
             {
                 path: 'list',
-                element: <Userlist />,
+                element: lazyCom(<Userlist />),
                 meta: {
                     title: '用户列表'
                 }
